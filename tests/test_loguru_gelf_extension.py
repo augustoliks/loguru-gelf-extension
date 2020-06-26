72cc1dd869cda5c5b3a65479804a799b5157d122
augustoliks <carlos.santos110@fatec.sp.gov.br>
2020-06-25 23:19:42 -0300

    test: basic tests

--- tests/test_loguru_gelf_extension.py
+++ tests/test_loguru_gelf_extension.py
@@ -1,5 +1,17 @@
-from gelfguru import __version__
+import gelfguru
+from loguru import logger
+import unittest
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestCallMethods(unittest.TestCase):
+    def setUp(self) -> None:
+        gelfguru.configure_gelf_output()
+
+    def test_loguru_calls(self):
+        logger.critical('test critical')
+        logger.error('test error')
+        logger.success('success == info')
+        logger.warning('test warning Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit')
+        logger.debug('test debug')
+        logger.trace('test test tests')
+        logger.emergency('emergency')
